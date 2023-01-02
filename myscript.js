fe3ac23857d8e3cf311e80f7b85da3592e29d0bb
Mat432 <mateja812@gmail.com>
2023-01-02 10:49:34 +0100

    Add index.html

---
+++ myscript.js
@@ -0,0 +1,5 @@
+//make overall look of the page 16x16div's make them within JS file
+//hoover effect over each div so it changes color 
+//add a button to open prompt with message with desired number of squares (limit to 100)
+
+
